2511c19f22a8f643bf133f1c9c400233e47fe4be
Hope Avalon <hopegag@gmail.com>
2022-10-16 18:46:38 +0100

    Added auth

--- src/main/java/com/avacorp/blog/controller/PostController.java
+++ src/main/java/com/avacorp/blog/controller/PostController.java
@@ -6,6 +6,7 @@ import com.avacorp.blog.service.PostService;
 import com.avacorp.blog.utils.AppConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -19,7 +20,7 @@ public class PostController {
     public PostController(PostService postService) {
         this.postService = postService;
     }
-
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto ) {
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
@@ -42,12 +43,13 @@ public class PostController {
     }
 
     //Update Post By ID
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto, @PathVariable(name = "id") long id) {
         PostDto postResponse = postService.updatePost(postDto, id);
         return new ResponseEntity<>(postResponse, HttpStatus.OK);
     }
-
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deletePost(@PathVariable(name = "id") long id){
         postService.deletePost(id);
--- src/main/java/com/avacorp/blog/payload/CommentDto.java
+++ src/main/java/com/avacorp/blog/payload/CommentDto.java
@@ -3,12 +3,24 @@ package com.avacorp.blog.payload;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Data
 public class CommentDto {
     private Long id;
-    private String name;
-    private String email;
-    private String body;
 
+    @NotEmpty
+    @Size(min = 3, message = "Name must not be empty")
+    private String name;
+
+    @NotEmpty
+    @Email(message = "Email must be valid")
+    private String email;
+
+    @NotEmpty
+    @Size(min = 5, message = "Body must have a valid response")
+    private String body;
 
 }
