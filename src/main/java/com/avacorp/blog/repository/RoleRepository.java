b60752f125ccb5866c056ec4094725ff89aecd0c
Hope Avalon <hopegag@gmail.com>
2022-10-17 08:12:02 +0100

    Added Roles and User

---
+++ src/main/java/com/avacorp/blog/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.avacorp.blog.repository;
+
+import com.avacorp.blog.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
