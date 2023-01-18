fab45576606c89fb8a21ae035bee56a749617dac
Avalon <hopegag@gmail.com>
2023-01-18 09:02:08 +0000

    Fix cours

--- src/main/java/com/avacorp/blog/AvacorpBlogRestApiApplication.java
+++ src/main/java/com/avacorp/blog/AvacorpBlogRestApiApplication.java
@@ -2,12 +2,27 @@ package com.avacorp.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AvacorpBlogRestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AvacorpBlogRestApiApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:8080")
+						.allowedMethods("POST", "PUT", "GET", "OPTIONS", "DELETE", "PATCH" ,"HEAD");
+			}
+		};
 	}
 
 }
